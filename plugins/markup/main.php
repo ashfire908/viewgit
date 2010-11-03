2c158d4b3cd7b07ca4d4750be3f7170f209f1bde
Andrew Hampe <andrew.hampe@gmail.com>
2010-11-03 03:50:53 -0500

    Polished up the markup plugin code; added textile support
    Polished up the markup plugin code; made it easier to add additional
    formats; added Textile support

--- plugins/markup/main.php
+++ plugins/markup/main.php
@@ -2,69 +2,88 @@
 
 class MarkupPlugin extends VGPlugin
 {
-	function __construct() {
-		global $conf;
-		if (isset($conf['markup'])) {
-			$this->register_action('render');
-			$this->register_hook('pagenav');
-		}
-	}
+    function __construct() {
+        global $conf;
+        if (isset($conf['markup'])) {
+            $this->register_action('render');
+            $this->register_hook('pagenav');
+        }
+    }
 
-	function action($action) {
-	    global $page;
-	    global $conf;
-	    
-    	$page['project'] = validate_project($_REQUEST['p']);
-    	$page['hash'] = validate_hash($_REQUEST['h']);
-    	$page['title'] = "$page[project] - Blob - ViewGit";
-    	if (isset($_REQUEST['hb'])) {
-    		$page['commit_id'] = validate_hash($_REQUEST['hb']);
-    	}
-    	else {
-    		$page['commit_id'] = 'HEAD';
-    	}
-    	$page['subtitle'] = "Render";
+    function action($action) {
+        global $page;
+        global $conf;
+        
+        $page['project'] = validate_project($_REQUEST['p']);
+        $page['hash'] = validate_hash($_REQUEST['h']);
+        $page['path'] = $_REQUEST['f'];
+        $page['title'] = "$page[project] - Blob - ViewGit";
+        if (isset($_REQUEST['hb'])) {
+            echo "Nope.";
+            $page['commit_id'] = validate_hash($_REQUEST['hb']);
+        }
+        else {
+            $page['commit_id'] = 'HEAD';
+        }
+        $page['subtitle'] = "Render";
     
-    	$page['path'] = '';
-    		$page['path'] = $_REQUEST['f']; // TODO validate?
-    
-    	// For the header's pagenav
-    	$info = git_get_commit_info($page['project'], $page['commit_id']);
-    	$page['commit_id'] = $info['h'];
-    	$page['tree_id'] = $info['tree'];
-    
-    	$page['pathinfo'] = git_get_path_info($page['project'], $page['commit_id'], $page['path']);
-    
-    	$page['data'] = fix_encoding(join("\n", run_git($page['project'], "cat-file blob $page[hash]")));
-    
-    	$page['lastlog'] = git_get_commit_info($page['project'], 'HEAD', $page['path']);
-    	
-    	// Render file
-    	if (preg_match('/\.md|mkdn?|mdown|markdown$/', $page['path']) > 0) {
-    	    // Markdown
-    	    include_once("markdown.php");
-    	    $page['html_data'] = Markdown($page['data']);
-    	}
-    	
-    	// Display page
-    	// Header
+        // For the header's pagenav
+        $info = git_get_commit_info($page['project'], $page['commit_id']);
+        $page['commit_id'] = $info['h'];
+        $page['tree_id'] = $info['tree'];
+        
+        $page['pathinfo'] = git_get_path_info($page['project'], $page['commit_id'], $page['path']);
+        
+        $page['data'] = fix_encoding(join("\n", run_git($page['project'], "cat-file blob $page[hash]")));
+        
+        $page['lastlog'] = git_get_commit_info($page['project'], 'HEAD', $page['path']);
+        
+        // Render file
+        switch ($this->match_file($page['path'])) {
+            case 'markdown': // Markdown
+                include_once('markdown.php');
+                $page['html_data'] = Markdown($page['data']);
+                return;
+            case 'textile':  // Textile
+                include_once('textile.php');
+                $textile = new Textile();
+                $page['html_data'] = $textile->TextileThis($page['data']);
+                return;
+            default:
+                $page['html_data'] = $page['data'];
+                return;
+        }
+        
+        // Display page
+        // Header
         require 'templates/header.php';
         $this->display_plugin_template('render', false);
         // Footer
         require 'templates/footer.php';
-	}
+    }
 
-	function hook($type) {
-		if ($type == 'pagenav') {
-			global $page;
-			if ($page['action'] == 'viewblob' or $page['action'] == 'render') {
-			    $request = array('a' => 'render',
-			                     'p' => $page['project'],
-			                     'h' => $page['hash'],
-			                     'f' => $page['path']);
-			    
-			    $page['links']['Render'] = $request;
-			}
-		}
-	}
+    function hook($type) {
+        if ($type == 'pagenav') {
+            global $page;
+            if (($page['action'] == 'viewblob' or $page['action'] == 'render')
+                and !$this->match_file($page['path'])) {
+                $request = array('a' => 'render',
+                                 'p' => $page['project'],
+                                 'h' => $page['hash'],
+                                 'f' => $page['path']);
+                
+                $page['links']['Render'] = $request;
+            }
+        }
+    }
+    
+    function match_file($path) {
+        if (preg_match('/\.md|mkdn?|mdown|markdown$/', $path) > 0) {
+            return 'markdown';
+        } elseif (preg_match('/\.textile$/', $path) > 0) {
+            return 'textile';
+        } else {
+            return false;
+        }
+    }
 }
